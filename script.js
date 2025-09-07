273c71981b1ed6104e5532fa124d87e0bbbca931
K4silv4 <joaaomarinoo.500@gmail.com>
2025-09-07 21:51:35 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const toggle = document.getElementById('menu-toggle');
 const nav = document.getElementById('nav');
+const navLinks = nav.querySelectorAll('a'); // todos los enlaces del menú
 
+// abrir/cerrar menú al pulsar hamburguesa
 toggle.addEventListener('click', () => {
   nav.classList.toggle('show');
 });
 
-const contactForm = document.getElementById('contactForm');
-const successMessage = document.getElementById('successMessage');
+// cerrar menú al pulsar un enlace
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    nav.classList.remove('show');
+  });
+});
 
 contactForm.addEventListener('submit', function(e) {
   e.preventDefault(); // Evita recargar la página
