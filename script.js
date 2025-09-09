13a7e1907182824bac7c17f6dc6d7d1b14720114
yungduck . <joaaomarinoo.500@gmail.com>
2025-09-09 20:07:15 +0200

    joder

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const toggle = document.getElementById('menu-toggle');
 const nav = document.getElementById('nav');
 const navLinks = nav.querySelectorAll('a'); // todos los enlaces del menú
@@ -76,36 +75,4 @@ document.addEventListener("DOMContentLoaded", () => {
       stopGame();
     }
   });
-=======
-const toggle = document.getElementById('menu-toggle');
-const nav = document.getElementById('nav');
-const navLinks = nav.querySelectorAll('a'); // todos los enlaces del menú
-
-// abrir/cerrar menú al pulsar hamburguesa
-toggle.addEventListener('click', () => {
-  nav.classList.toggle('show');
-});
-
-// cerrar menú al pulsar un enlace
-navLinks.forEach(link => {
-  link.addEventListener('click', () => {
-    nav.classList.remove('show');
-  });
-});
-
-contactForm.addEventListener('submit', function(e) {
-  e.preventDefault(); // Evita recargar la página
-
-  // Para usar mailto o Formspree, dejamos que se envíe después
-  this.submit();
-
-  // Mostrar mensaje de éxito
-  successMessage.style.display = 'block';
-
-  // Limpiar formulario después de 3 segundos
-  setTimeout(() => {
-    successMessage.style.display = 'none';
-    contactForm.reset();
-  }, 3000);
->>>>>>> f6d88c34e6dca1c7c5fa609aec7cc8cbce7171cc
 });
